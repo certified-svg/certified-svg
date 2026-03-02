78f8cbe3d450636b1c45a1dd620dc89f61a6a7d0
certified-svg <jboyking44@gmail.com>
2026-03-02 12:59:35 -0600

    Add files via upload

---
+++ # Information Secuirty Group Assignment.py
@@ -0,0 +1,42 @@
+# Information Secuirty Group Assignment
+ 
+ 
+
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad 
+import base64
+
+key = b"thisisasecretkey"
+
+message = input("Enter the maeesage you want to encrypt: ")
+
+data = message.encode()
+
+#print(type(message))
+#print(type(data))
+
+cipher = AES.new(key, AES.MODE_CBC)
+
+iv = cipher.iv
+
+padded_data = pad(data, AES.block_size)
+
+ciphertext = cipher.encrypt(padded_data)
+
+encrypted = base64.b64encode(iv + ciphertext)
+print("Enrypted:", encrypted.decode())
+
+
+encrypted = base64.b64encode(iv + ciphertext)
+raw = base64.b64decode(encrypted)
+iv = raw[:16]
+ciphertext = raw[16:]
+
+cipher = AES.new(key, AES.MODE_CBC, iv)
+
+decrypted_padded = cipher.decrypt(ciphertext)
+
+decrypted = unpad(decrypted_padded, AES.block_size)
+
+print("Decrypted:", decrypted.decode())
